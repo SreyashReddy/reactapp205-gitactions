ad9e1692ef31e7f04aa742f4624f98ded68e6d5f
SreyashReddy <polisreyash@gmail.com>
2025-07-28 10:02:42 +0530

    react project version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css'
+import './style.css'
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - Sreyu</h1>
+      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
