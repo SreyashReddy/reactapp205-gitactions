bae086855cbafcb226f1a9ba5b51c7e460913e93
SreyashReddy <polisreyash@gmail.com>
2025-07-29 12:34:41 +0530

    added aline

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Welcome to My React Project - Sreyu</h1>
       <p>CI/CD Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Github Actions.</p>
+      <p>Done with Git Pages & Git Action</p>
       <p></p>
     </div>
   );
